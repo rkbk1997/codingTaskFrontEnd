a31cb6dab98f03b05580af6e2f59097f9bbbcf51
Ravi Kumar <ravi.kumar1@daffodilsw.com>
2021-09-07 14:16:32 +0530

    Add login auth

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CommonService } from '../common.service';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-auth',
@@ -10,7 +11,7 @@ import { CommonService } from '../common.service';
 export class AuthComponent implements OnInit {
   formType: any ;
   inputType : any = 'password'  
-  constructor(private activatedRoute: ActivatedRoute, private commonservice : CommonService) { }
+  constructor(private activatedRoute: ActivatedRoute, private commonservice : CommonService, private router: Router) { }
 
   ngOnInit(): void {
     this.activatedRoute.data.subscribe( data => {
@@ -29,10 +30,63 @@ export class AuthComponent implements OnInit {
       )
     }else{
       this.commonservice.registations(value).subscribe(
-        res => console.log(res),
-        err => console.log(err),        
+        res =>{
+          if(res){
+            Swal.fire({
+              title: 'Thanks for connect with us ',
+              text: '',
+              icon: 'success',
+              showCancelButton: false,
+              confirmButtonText: 'Press Here For Login ',
+              cancelButtonText: ''
+            }).then((result) => {
+              if (result.isConfirmed) {
+                this.router.navigate(['/']);
+              }
+            })
+          }
+        },
+        err => {
+          Swal.fire({
+            title: 'Oops ! something went worng ',
+            text: '',
+            icon: 'warning',
+            showCancelButton: false,
+            confirmButtonText: 'Press Here For Register yourSelf ',
+            cancelButtonText: ''
+          }).then((result) => {
+            if (result.isConfirmed) {
+              this.router.navigate(['/register']);
+            }
+          })
+        }
       );
     }
+  }
+
+  showMessage(){
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'You will not be able to recover this imaginary file!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, delete it!',
+      cancelButtonText: 'No, keep it'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Deleted!',
+          'Your imaginary file has been deleted.',
+          'success'
+        )
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire(
+          'Cancelled',
+          'Your imaginary file is safe :)',
+          'error'
+        )
+      }
+    })
   }
 
 }
