7360b4c8bd0e64e1cc09cb74a35e00c9435baf2a
Ravi Kumar <ravi.kumar1@daffodilsw.com>
2021-09-07 14:28:34 +0530

    Add login auth

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -77,30 +77,4 @@ export class AuthComponent implements OnInit {
       );
     }
   }
-
-  showMessage(){
-    Swal.fire({
-      title: 'Are you sure?',
-      text: 'You will not be able to recover this imaginary file!',
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonText: 'Yes, delete it!',
-      cancelButtonText: 'No, keep it'
-    }).then((result) => {
-      if (result.isConfirmed) {
-        Swal.fire(
-          'Deleted!',
-          'Your imaginary file has been deleted.',
-          'success'
-        )
-      } else if (result.dismiss === Swal.DismissReason.cancel) {
-        Swal.fire(
-          'Cancelled',
-          'Your imaginary file is safe :)',
-          'error'
-        )
-      }
-    })
-  }
-
 }
